00ea6d59198a3fa6660129c256060fcad5657ba0
aidan <aidansavantgeneral@gmail.com>
2026-05-19 17:28:47 -0600

    Reordered stuff, sorry...

---
+++ services/game_service/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
+
+int main() {
+    spdlog::info("Starting ET Game Service...");
+
+}
---
+++ services/game_service/models/types.hpp
@@ -0,0 +1,47 @@
+#ifndef ET_GAME_SERVICE_TYPES_HPP
+#define ET_GAME_SERVICE_TYPES_HPP
+
+#include <string>
+
+namespace et_game {
+using RoomId = std::string;
+using TileId = unsigned char;
+using PickupId = std::string;
+using ItemTypeId = std::string;
+using TilesetName = std::string;
+
+enum class Direction {
+    North,
+    South,
+    East,
+    West
+};
+
+struct TilePos {
+    int x = 0;
+    int y = 0;
+
+    friend inline bool operator==(const TilePos& lhs, const TilePos& rhs) {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
+
+    friend inline bool operator!=(const TilePos& lhs, const TilePos& rhs) {
+        return !(lhs == rhs);
+    }
+};
+
+struct Vec2 {
+    float x = 0.0f;
+    float y = 0.0f;
+
+    friend inline bool operator==(const Vec2& lhs, const Vec2& rhs) {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
+
+    friend inline bool operator!=(const Vec2& lhs, const Vec2& rhs) {
+        return !(lhs == rhs);
+    }
+};
+}; // namespace et_game
+
+#endif
